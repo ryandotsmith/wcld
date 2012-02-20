cee64ef1f35d58e4e67f42378d25e7af4eed8a47
Ryan R. Smith (ace hacker) <this.ryansmith@gmail.com>
2012-02-20 15:51:03 -0800

    only parse values that match a k=v sig

--- wcld.go
+++ wcld.go
@@ -13,19 +13,21 @@ import (
 
 var pg *sql.DB
 
+var kvSig = regexp.MustCompile(`([a-zA-Z0-9\.\_\-\:\/])=([a-zA-Z0-9\.\_\-\:\/\"])`)
+var kvData = regexp.MustCompile(`([a-zA-Z0-9\.\_\-\:\/]+)(=?)("[^"]+"|[a-zA-Z0-9\.\_\-\:\/]*)`)
+var timeSig = regexp.MustCompile(`(\d\d\d\d)(-)?(\d\d)(-)?(\d\d)(T)?(\d\d)(:)?(\d\d)(:)?(\d\d)(\.\d+)?(Z|([+-])(\d\d)(:)?(\d\d))`)
+
 func main() {
 	var err error
 	pg, err = sql.Open("postgres", os.Getenv("DATABASE_URL"))
 	if err != nil {
 		log.Fatalf("error=true action=db_conn message=%v", err)
 	}
-
 	log.Println("bind tcp", os.Getenv("PORT"))
 	server, err := net.Listen("tcp", "0.0.0.0:"+os.Getenv("PORT"))
 	if err != nil {
 		log.Fatalf("error=true action=net_listen message=%v", err)
 	}
-
 	conns := clientConns(server)
 	for {
 		go readData(<-conns)
@@ -60,8 +62,8 @@ func readData(client net.Conn) {
 
 func handleInput(logLine string) {
 	log.Printf("action=handleInput logLine=%v", logLine)
-	logData := toHstore(trimKeys(logLine))
-	logTime := parseTime(logLine)
+	logTime := timeSig.FindString(logLine)
+	logData := toHstore(logLine)
 	if len(logData) > 0 {
 		log.Printf("action=insert logData=%v logTime=%v", logData, logTime)
 		_, err := pg.Exec("INSERT INTO log_data (data, time) VALUES ($1::hstore, $2)", logData, logTime)
@@ -72,25 +74,25 @@ func handleInput(logLine string) {
 	return
 }
 
-func parseTime(logLine string) (time string) {
-	t, _ := regexp.Compile(`(\d\d\d\d)(-)?(\d\d)(-)?(\d\d)(T)?(\d\d)(:)?(\d\d)(:)?(\d\d)(\.\d+)?(Z|([+-])(\d\d)(:)?(\d\d))`)
-	time = t.FindString(logLine)
-	log.Printf("action=parse_time t=%v", time)
+func toHstore(logLine string) (res string) {
+	if kvSig.FindAllString(logLine, -1) != nil {
+		kvs := trimKeys(logLine)
+		res = strings.Replace(kvs, "=", "=>", -1)
+	} else {
+		res = ""
+	}
 	return
 }
 
-func toHstore(kvs string) string {
-	return strings.Replace(kvs, "=", "=>", -1)
-}
-
 func trimKeys(logLine string) (kvs string) {
-	kv, _ := regexp.Compile(`([a-z0-9_.-]+)=("[^"]+"|[a-z0-9_.-]+)`)
-	pairs := kv.FindAllString(logLine, -1)
-	max := len(pairs) - 1
-
-	for i, elt := range pairs {
-		log.Printf("action=process_element elt=%v position=%v max=%v", elt, i, max)
-		kvs += elt
+	words := kvData.FindAllString(logLine, -1)
+	max := len(words) - 1
+	for i, elt := range words {
+		if strings.Contains(elt, "=") {
+			kvs += elt
+		} else {
+			kvs += elt + "=true"
+		}
 		if i != max {
 			kvs += ", "
 		}
--- wcld_test.go
+++ wcld_test.go
@@ -4,50 +4,56 @@ import (
 	"testing"
 )
 
+func TestMakeKeys(t *testing.T) {
+	actual := trimKeys("hello world")
+	expected := "hello=true, world=true"
+
+	if actual != expected {
+		t.Errorf("expected(%v) actual(%v)", expected, actual)
+	}
+}
+
 func TestTrimKeys(t *testing.T) {
-	actual := trimKeys(`#test name=ryan age=25 height-feet=6 height-inches=5 weight_lbs=210 _ssn=123 fav_quote="oh hai"`)
+	actual := trimKeys(`name=ryan age=25 height-feet=6 height-inches=5 weight_lbs=210 _ssn=123 fav_quote="oh hai"`)
 	expected := `name=ryan, age=25, height-feet=6, height-inches=5, weight_lbs=210, _ssn=123, fav_quote="oh hai"`
 
 	if actual != expected {
-		t.Errorf("expected %v actual: %v", expected, actual)
+		t.Errorf("\n e(%v) \n a(%v)", expected, actual)
 	}
 }
 
 func TestToHstore(t *testing.T) {
-	trimed := trimKeys("#test name=ryan age=25")
-	actual := toHstore(trimed)
-	expected := "name=>ryan, age=>25"
+	actual := toHstore("test name=ryan age=25")
+	expected := "test=>true, name=>ryan, age=>25"
 
 	if actual != expected {
-		t.Errorf("expected %v actual: %v", expected, actual)
+		t.Errorf("\n e(%v) \n a(%v)", expected, actual)
 	}
 }
 
 func TestToHstoreOnRouterLine(t *testing.T) {
-	trimed := trimKeys("PUT shushu.herokuapp.com/resources/584093/billable_events/40531647 dyno=web.3 queue=0 wait=0ms service=52ms status=201 bytes=239")
-	actual := toHstore(trimed)
-	expected := "dyno=>web.3, queue=>0, wait=>0ms, service=>52ms, status=>201, bytes=>239"
+	actual := toHstore("PUT shushu.herokuapp.com/resources/584093/billable_events/40531647 dyno=web.3 queue=0 wait=0ms service=52ms status=201 bytes=239")
+	expected := "PUT=>true, shushu.herokuapp.com/resources/584093/billable_events/40531647=>true, dyno=>web.3, queue=>0, wait=>0ms, service=>52ms, status=>201, bytes=>239"
 
 	if actual != expected {
-		t.Errorf("expected %v actual: %v", expected, actual)
+		t.Errorf("\n e(%v) \n a(%v)", expected, actual)
 	}
 }
 
 func TestToHstoreOnSQLLine(t *testing.T) {
-	trimed := trimKeys(`DEBUG: (0.000863s) INSERT INTO "billable_events" ("provider_id", "rate_code_id", "entity_id", "hid", "qty", "product_name", "time", "state", "created_at") VALUES (5, 2, '40531942', '369504', 1, 'worker', '2012-02-13 18:36:30.000000+0000', 'open', '2012-02-13 18:36:49.810784+0000') RETURNING *`)
-	actual := toHstore(trimed)
+	actual := toHstore(`DEBUG: (0.000863s) INSERT INTO "billable_events" ("provider_id", "rate_code_id", "entity_id", "hid", "qty", "product_name", "time", "state", "created_at") VALUES (5, 2, '40531942', '369504', 1, 'worker', '2012-02-13 18:36:30.000000+0000', 'open', '2012-02-13 18:36:49.810784+0000') RETURNING *`)
 	expected := ""
 
 	if actual != expected {
-		t.Errorf("expected %v actual: %v", expected, actual)
+		t.Errorf("\n e(%v) \n a(%v)", expected, actual)
 	}
 }
 
 func TestParseTime(t *testing.T) {
-	actual := parseTime("<13>1 2012-02-14T00:44:30+00:00 d.39c761b5-2e3a-4f93-9e68-2549c85650e2 app web.4 - - INFO: provider=3 #api_prepare_body key=value")
+	actual := timeSig.FindString("<13>1 2012-02-14T00:44:30+00:00 d.39c761b5-2e3a-4f93-9e68-2549c85650e2 app web.4 - - INFO: provider=3 #api_prepare_body key=value")
 	expected := "2012-02-14T00:44:30+00:00"
 
 	if actual != expected {
-		t.Errorf("expected %v actual: %v", expected, actual)
+		t.Errorf("\n e(%v) \n a(%v)", expected, actual)
 	}
 }
